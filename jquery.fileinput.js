aace09ad8fa4b34eaf0d6974062a15a738937c98
Hannu Leinonen <hleinone@gmail.com>
2011-12-29 19:08:06 +0200

    Modified package.json to match jQuery plugins site's requirements.

--- jquery.fileinput.js
+++ jquery.fileinput.js
@@ -1,7 +1,7 @@
 /**
- * {{= pkg.name}} v{{= pkg.version}}
+ * {{= pkg.title}} v{{= pkg.version}}
  *
- * Copyright 2011, {{= pkg.author}}
+ * Copyright 2011, {{= pkg.author.name}} <{{= pkg.author.email}}>
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
